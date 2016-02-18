d974c7e0e013637e155a1473e7ba855feec6148a
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-02-18 14:23:19 -0500

    use tail -F to watch new log file content

--- clicks-reporter.py
+++ clicks-reporter.py
@@ -4,8 +4,11 @@ import argparse
 import datetime
 import hashlib
 import json
+import os
 import random
 import re
+import select
+import subprocess
 import sys
 import urllib
 import urllib2
@@ -33,38 +36,68 @@ def generate_site_query_id(self, query):
     return hashlib.sha1(query).hexdigest()
 
 
-def parse_log_file(log_file_path):
+def process_line(line):
+    m = re.search('GET (.*) HTTP', line)
+    if not m:
+        return
+    url = m.group(1)
+    pr = urlparse.urlparse(url)
+    qs = pr.query
+    if not qs:
+        return
+    query = urlparse.parse_qs(qs)
+    if 'osm' not in query:
+        return
+    print query
+
+
+def process_log_file(date, log_file_path):
     print log_file_path
 
-    with open(log_file_path) as f:
-        for line in f:
-            m = re.search('GET (.*) HTTP', line)
-            if not m:
-                continue
-            url = m.group(1)
-            pr = urlparse.urlparse(url)
-            qs = pr.query
-            if not qs:
-                continue
-            query = urlparse.parse_qs(qs)
-            if 'osm' not in query:
-                continue
-            print query
+    f = subprocess.Popen(['tail', '-F', log_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = select.poll()
+    p.register(f.stdout)
+
+    while True:
+        if not p.poll(10 * 1000) and date != datetime.date.today():
+            break
+        line = f.stdout.readline()
+        process_line(line)
+
+
+def process(logdir, date):
+    print 'Processing', date.isoformat()
+
+    log_file_path = logdir + 'localhost_access_log.' + date.isoformat() + '.txt'
+    if not os.path.exists(log_file_path):
+        print log_file_path, 'does not exist'
+        return
+
+    process_log_file(date, log_file_path)
 
 
 def main():
-    logs_dir = '/usr/local/tomcat-solr/logs/'
-    date_str = datetime.date.today().isoformat()
-    log_file_path = logs_dir + 'localhost_access_log.' + date_str + '.txt'
-    parse_log_file(log_file_path)
-
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='"Parse CiteSeerX access log and report user clicks to TREC OpenSearch')
-    parser.add_argument('-k', '--key', type=str, help='Provide a user key.')
+    parser.add_argument('-s', '--date', type=str, help='Start from date in format YYYY-MM-DD, default to today')
+    parser.add_argument('-k', '--key', type=str, help='Provide a user key')
     parser.add_argument('-p', '--port', help='Port number of OpenSearch API server')
-#    parser.add_argument('-d', '--logdir', required=True, help='Tomcat logs directory')
+    parser.add_argument('-d', '--logdir', required=True, help='Tomcat logs directory')
 
     args = parser.parse_args()
+    if args.date:
+        date = datetime.datetime.strptime(args.date, '%Y-%m-%d').date()
+    else:
+        date = datetime.date.today()
     KEY = args.key
     #port = int(args.port)
+
+    if not os.path.exists(args.logdir):
+        print args.logdir, 'does not exist'
+        return
+
+    while date <= datetime.date.today():
+        process(args.logdir, date)
+        date += datetime.timedelta(1)
+
+if __name__ == "__main__":
     main()

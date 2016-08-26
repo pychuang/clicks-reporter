d208ef433c16dce25c25e4bbc8053db02b4c04c2
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-08-26 12:34:28 -0400

    clicks-reporter.py: upload doc to OpenSearch if needed

--- clicks-reporter.py
+++ clicks-reporter.py
@@ -7,7 +7,6 @@ import datetime
 import hashlib
 import json
 import os
-import random
 import re
 import requests
 import select
@@ -15,14 +14,14 @@ import subprocess
 import time
 import sys
 import urllib
-import urllib2
 import urlparse
-import web
 
 
-urls = (
-  '/select', 'select'
-)
+opensearch_url = ''
+opensearch_key = ''
+repo_url = ''
+solr_url = ''
+logdir = ''
 
 
 def cleanup(query):
@@ -35,6 +34,7 @@ def cleanup(query):
 
 def generate_site_query_id(query):
     return hashlib.sha1(query).hexdigest()
+
 
 def convert_feedback_format(feedback):
     os_feedback = {
@@ -56,11 +56,12 @@ def convert_feedback_format(feedback):
         doclist.append(d)
     return os_feedback
 
-def report_feedback(opensearch_url, key, feedback):
+
+def report_feedback(feedback):
     sid = feedback['sid']
     data = convert_feedback_format(feedback)
     # PUT /api/site/feedback/(key)/(sid)
-    url = '/'.join([opensearch_url, 'api/site/feedback', key, sid])
+    url = '/'.join([opensearch_url, 'api/site/feedback', opensearch_key, sid])
     print "URL: %s" % url
     data_json = json.dumps(data)
     #data_json = json.dumps(data, indent=4, separators=(',', ': '))
@@ -85,7 +86,111 @@ def report_feedback(opensearch_url, key, feedback):
             continue
         break
 
-def process_line(opensearch_url, key, feedbacks, line):
+
+def get_doc_text_from_repo(doi):
+    global repo_url
+
+    params = {
+        'repid': 'rep1',
+        'type': 'txt',
+        'doi': doi,
+    }
+
+    r = requests.get(repo_url, params=params)
+    if r.status_code != 200:
+        print "Filaed to get text of %s from repository" % doi
+        return None
+    return r.text
+
+
+def get_doc_from_solr(doi):
+    global solr_url
+
+    params = {
+        'q': '*:*',
+        'wt': 'json',
+        'fq': 'doi:' + doi,
+    }
+    r = requests.get(solr_url, params=params)
+    if r.status_code != 200:
+        return None
+    result = r.json()
+    response = result['response']
+    docs = response['docs']
+    if not docs:
+        print "Cannot find %s in Solr" % doi
+        return None
+    return docs[0]
+
+
+def put_doc_to_opensearch(title, text, doi):
+    doc = {
+        'site_docid': doi,
+        'title': title,
+        'content': {
+            'text': text,
+        }
+    }
+    #print doc
+
+    # PUT /api/site/doc/(key)/(site_docid)
+    url = '/'.join([opensearch_url, 'api/site/doc', opensearch_key, doi])
+    print "PUT %s" % url
+    data_json = json.dumps(doc)
+    try:
+        r = requests.put(url, data=data_json)
+        print r
+    except Exception as e:
+        print e
+        print "Failed put %s to OpenSearch" % doi
+        return False
+    return True
+
+
+def upload_doc(doi):
+    doc = get_doc_from_solr(doi)
+    if not doc:
+        return False
+
+    if 'title' in doc:
+        title = doc['title']
+    else:
+        title = ''
+
+    if 'abstract' in doc:
+        abstract = doc['abstract']
+    else:
+        abstract = ''
+
+    text = get_doc_text_from_repo(doi)
+    if not text:
+        text = abstract
+
+    # Due to the 16MB limitation of BSON documents in MongoDB,
+    # we need to trim down large documents in data set.
+    encoded_text = text.encode('utf-8')[:16 * 1024 * 1024]
+    text = encoded_text.decode('utf-8', 'ignore')
+
+    return put_doc_to_opensearch(title, text, doi)
+
+
+doc_set = set()
+
+def upload_doc_if_necessary(doi):
+    global doc_set
+
+    if doi in doc_set:
+        print "%s already in OpenSearch" % doi
+        return
+
+    print "%s not in OpenSearch database, upload it" % doi
+    if upload_doc(doi):
+        doc_set.add(doi)
+        with open('docs.txt', 'a') as f:
+            f.write("%s\n" % doi)
+
+
+def process_line(feedbacks, line):
     m = re.search('GET (.*) HTTP', line)
     if not m:
         return
@@ -125,6 +230,7 @@ def process_line(opensearch_url, key, feedbacks, line):
             continue
         (r, doi, t) = marker.split(':')
         ranking  = int(r)
+        upload_doc_if_necessary(doi)
         if ranking not in docs:
             doc = {
                 'doi': doi,
@@ -143,10 +249,10 @@ def process_line(opensearch_url, key, feedbacks, line):
         return
     docs[rank]['clicked'] = True
 
-    report_feedback(opensearch_url, key, feedback)
+    report_feedback(feedback)
 
 
-def process_log_file(opensearch_url, key, date, log_file_path):
+def process_log_file(date, log_file_path):
     print log_file_path
 
     f = subprocess.Popen(['tail', '-F', '-n', '+0', log_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -158,14 +264,14 @@ def process_log_file(opensearch_url, key, date, log_file_path):
     while True:
         if p.poll(timeout):
             line = f.stdout.readline()
-            process_line(opensearch_url, key, feedbacks, line)
+            process_line(feedbacks, line)
         elif date != datetime.date.today():
             break
 
     f.kill()
 
 
-def process(opensearch_url, key, logdir, date):
+def process(date):
     print 'Processing', date.isoformat()
 
     log_file_path = logdir + 'localhost_access_log.' + date.isoformat() + '.txt'
@@ -173,15 +279,29 @@ def process(opensearch_url, key, logdir, date):
         print log_file_path, 'does not exist'
         return False
 
-    process_log_file(opensearch_url, key, date, log_file_path)
+    process_log_file(date, log_file_path)
     return True
 
 
 def main():
+    global opensearch_url
+    global opensearch_key
+    global repo_url
+    global solr_url
+    global logdir
+    global doc_set
+
+    with open('docs.txt') as f:
+        for line in f:
+            doi = line.strip()
+            doc_set.add(doi)
+
     config = ConfigParser.ConfigParser()
     config.read('config.ini')
     opensearch_url = config.get('opensearch', 'url')
     opensearch_key = config.get('opensearch', 'key')
+    repo_url = config.get('repo', 'url')
+    solr_url = config.get('solr', 'url')
     logdir = config.get('tomcat', 'log-dir')
 
     parser = argparse.ArgumentParser(description='"Parse CiteSeerX access log and report user clicks to TREC OpenSearch')
@@ -198,7 +318,7 @@ def main():
         return
 
     while date <= datetime.date.today():
-        if not process(opensearch_url, opensearch_key, logdir, date) and date == datetime.date.today():
+        if not process(date) and date == datetime.date.today():
             time.sleep(60)
             continue
         date += datetime.timedelta(1)
